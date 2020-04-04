1eb1b0f904fce5ac9700eefe7ccd0b66dee538f8
Regina Musovirova <musovirova@yahoo.com>
2020-04-03 20:42:40 -0400

    tested app & fix error

--- app.js
+++ app.js
@@ -5,19 +5,164 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 ​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+//const OUTPUT_DIR = path.resolve(__dirname, "output")
+c//onst outputPath = path.join(OUTPUT_DIR, "team.html");
 ​
-const render = require("./lib/htmlRenderer");
+//const render = require("./lib/htmlRenderer");
 ​
 ​
+async function start() {
+    console.log("Let's make your Dream team!")
 
-​
+    //Set Variable to hold HTML
+    let teamHTML = "";
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
+    //Variable to hold number of team members
+    let teamSize;
 
+    //First Q to ask to set up loop
+    await inquirer.prompt(
+        {
+            type: "number",
+            message: "How many people are in your team?",
+            name: "noOfTeamMem"
+        }
+    )
+    .then((data) => {
+        // Number of team members placed in teamSize for scope purposes.
+        // 1 is added start from 1 rather than 0 for user understanding.
+        teamSize = data.noOfTeamMem + 1;
+
+    });
+
+    //If Team Size is 0, will and program
+    if (teamSize === 0) {
+        console.log("There is no one in your team...")
+        return;
+    }
+
+    //Loop begins to ask questions depending on the size
+    for(i = 1; i < teamSize; i++){
+
+        //Global variables set
+        let name;
+        let id;
+        let title;
+        let email;
+
+        //Prompts user to answer the basic questions of the employee
+        await inquirer.prompt([ 
+            {
+                type: "input",
+                message: `What is employee (${i})'s name?`,
+                name: "name"
+            },
+            {
+                type: "input",
+                message: `What is the employee (${i})'s id?`,
+                name: "id"
+            },
+            {
+                type: "input",
+                message: `What is the employee (${i})'s Email?`,
+                name: "email"
+            },
+            {
+                type: "list",
+                message: `what the employee (${i})'s title?`,
+                name: "title",
+                choices: ["Engineer", "Intern", "Manager"]
+            }
+        ])
+        .then((data) => {
+            // Takes data from user and places value in global variables
+            name = data.name;
+            id = data.id;
+            title = data.title;
+            email = data.email;
+        });
+
+        // Switch Case depending on the title of the employee
+        switch (title){
+            case "Manager":
+
+                // ask user of Manager's Office Number
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your Manager's Office Number?",
+                        name: "officeNo"
+                    }
+                ])
+                .then((data) => {
+
+                    // Create a new object with all avaiable user input data
+                    const manager = new Manager(name, id, email, data.officeNo);
+
+                    // Reads and places HTML from manager.html in teamMemever Variable
+                    teamMember = fs.readFileSync("templates/manager.html");
+
+                    // Uses eval() to pass template literals from html files.
+                    // Adds the string to the team HTML.
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+            //Steps Similar to Manager but for intern
+            case "Intern":
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What school is your Intern attending?",
+                        name: "school"
+                    }
+                ])
+                .then((data) => {
+                    const intern = new Intern(name, id, email, data.school);
+                    teamMember = fs.readFileSync("templates/intern.html");
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+            //Steps Similar to Manager but for engineer
+            case "Engineer":
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your Engineer's GitHub?",
+                        name: "github"
+                    }
+                ])
+                .then((data) => {
+                    const engineer = new Engineer(name, id, email, data.github);
+                    teamMember = fs.readFileSync("templates/engineer.html");
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+        } // End of Switch Case
+
+    } // End of For loop
+
+    // Reads employee.html and places html in a variable
+    const mainHTML = fs.readFileSync("templates/employee.html");
+    
+    // Use eval to impement html from employee
+    teamHTML = eval('`'+ mainHTML +'`');
+
+    // write file to new team.html file
+    fs.writeFile("output/team.html", teamHTML, function(err) {
+
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success!");
+      
+      });
+
+    // console.log(teamHTML);
+}
+
+
+start();
